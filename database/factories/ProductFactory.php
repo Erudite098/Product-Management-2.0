e4e5fc4f0689b7e6d9520dd647fb1d11105b38fd
Erudite098 <macalde.kerzania1029@gmail.com>
2024-11-20 19:05:29 +0800

    initial commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+const randomCategory = ['Fashion & Apparel',
+                        'Electronics & Gadgets',
+                        'Health & Beauty',
+                        'Home & Living',
+                        'Toys & Baby Products',
+                        'Sports & Outdoor Equipment',
+                        'Books & Media',
+                        'Groceries & Food Products',
+                        'Pet Supplies',
+                        'Automotive Parts & Accessories'];
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //Malabanannn
+            'barcode' => fake()->numberBetween(1000000000, 9999999999),
+            'product_name' => fake()->words(3, true), 
+            'description' => fake()->sentence(),
+            'price' => fake()->randomFloat(2, 10, 1000),
+            'quantity' => fake()->numberBetween(1, 100),
+            'category' => fake()->randomElement(randomCategory)
+
+
+        ];
+    }
+}
